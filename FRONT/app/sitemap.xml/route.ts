0b659d23ecd2993052466ad734fa68b5101c20ba
solzarr <marcustorvalds.se@gmail.com>
2025-06-05 13:25:17 +0200

    sitemap

--- FRONT/app/sitemap.xml/route.ts
+++ FRONT/app/sitemap.xml/route.ts
@@ -1,14 +1,21 @@
 export async function GET() {
-  const body = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>https://johan-monard-devsite.vercel.app/</loc></url>
-    <url><loc>https://johan-monard-devsite.vercel.app/about</loc></url>
-    <url><loc>https://johan-monard-devsite.vercel.app/contact</loc></url>
-  </urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://johan-monard-devsite.vercel.app/</loc>
+  </url>
+  <url>
+    <loc>https://johan-monard-devsite.vercel.app/about</loc>
+  </url>
+  <url>
+    <loc>https://johan-monard-devsite.vercel.app/contact</loc>
+  </url>
+</urlset>`;
 
-  return new Response(body, {
+  return new Response(sitemap, {
+    status: 200,
     headers: {
-      "Content-Type": "application/xml",
+      'Content-Type': 'application/xml',
     },
   });
 }
