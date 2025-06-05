f58d0bc1ef5ce4f6cbe5234e5d9597cb39caf3c2
solzarr <marcustorvalds.se@gmail.com>
2025-06-05 13:03:15 +0200

    sitemap

---
+++ FRONT/app/sitemap.xml/routes.ts
@@ -0,0 +1,24 @@
+// app/sitemap.xml/route.ts
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://johan-monard-devsite.vercel.app/</loc>
+    </url>
+    <url>
+      <loc>https://johan-monard-devsite.vercel.app/about</loc>
+    </url>
+    <url>
+      <loc>https://johan-monard-devsite.vercel.app/contact</loc>
+    </url>
+  </urlset>`;
+
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
